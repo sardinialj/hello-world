2fd9b27c844b62527b638a15f5c9b8f9d797a9d2
sardinialj <89537121+sardinialj@users.noreply.github.com>
2021-08-25 12:15:11 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/***************************************************************
+* CMSC 255 Lab 1
+****************************************************************
+* Luke Sardinia
+* 08/25/2021
+* CMSC 255 004
+****************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("<Luke Sardinia>");
+		System.out.println("<CMSC 255 Lab 1>");
+		System.out.println("<Lab 1>");
+		System.out.println("<08/25/2021>");
+		System.out.println("<CMSC 255 004>");
+		System.out.println();
+		
+	}
+}	
